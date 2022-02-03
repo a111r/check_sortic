d2faf3960107c9e0d0bdd84a8962cedd81844885
a111r <92301153+a111r@users.noreply.github.com>
2022-02-03 11:21:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include"check_sortic.h"
+
+int main()
+{
+    HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(Console, FOREGROUND_INTENSITY);
+    SetConsoleTextAttribute(Console, FOREGROUND_GREEN ); // green
+    SetConsoleTextAttribute(Console, 15 ); // white
+    string str, mass;
+    ifstream file("test.txt");
+    while(file)
+    {
+        vector <int> a;
+        vector <int> b;
+        getline(file, str);
+        pause(str, a, b);
+        mass = Issortic(a);
+
+        if(mass == "OK")
+        {
+             SetConsoleTextAttribute(Console, FOREGROUND_GREEN ); // green
+             cout << mass << endl;
+        }
+        else if(mass == "KO")
+        {
+             SetConsoleTextAttribute(Console, 15 ); // white
+             cout << mass << endl;
+        }
+        SetConsoleTextAttribute(Console, 15 ); // white
+    }
+    return 0;
+}
